8822ebdf7570a4f1ff9da4ca4d4e0dfb41d0a5f1
Bryam Guzmán <46763361+bguzman012@users.noreply.github.com>
2020-12-17 23:04:40 -0500

    Revert "Revert "Merge pull request #2 from bguzman012/chuchuca""
    This reverts commit d50f187bd4cfab36bc6d4148424f39e465a1a06e.

--- src/main/java/astronet/ec/modelo/Cliente.java
+++ src/main/java/astronet/ec/modelo/Cliente.java
@@ -204,6 +204,30 @@ public class Cliente implements Serializable {
 		this.codigoAntenaTemp = codigoAntenaTemp;
 	}
 
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellidos=" + apellidos
